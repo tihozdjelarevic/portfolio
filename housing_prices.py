71953c05ce069bb59b81f3fa43f77ed1aa788ac6
Felipe Marineli <35429973+fmarineli@users.noreply.github.com>
2019-04-16 20:47:05 +0200

    Add files via upload

---
+++ housing_prices.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+iowa_file_path = './input/train.csv'
+home_data = pd.read_csv(iowa_file_path)
+
+# Target object (sale price)
+y = home_data.SalePrice
+# X is created with the columns that will be used to train our model
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model. Here we are using the DecisionTreeRegressor()
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# We define the funcion get_mae() to to iterate over our prediction model in order to determine the best value for max_leaf_nodes
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+# Using best value for max_leaf_nodes
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# In an attempt to improve our predictions, we implement the RandomForestRegressor() model to compare its accuracy with the DecisionTreeRegressor() model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+# The Random Forest model is better. A new Random Forest model is created to be applied to the whole data
+rf_model_on_full_data = RandomForestRegressor(random_state=1)
+rf_model_on_full_data.fit(X, y)
+
+# The data that will be used to test the model
+test_data_path = './input/test.csv'
+test_data = pd.read_csv(test_data_path)
+test_X = test_data[features]
+
+test_preds = rf_model_on_full_data.predict(test_X)
+
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': test_preds})
+output.to_csv('result.csv', index=False)
